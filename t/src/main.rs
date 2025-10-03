5e07f867211d4e283c17bd6bba8638a80fb6a4b2
郭申申 <3501641633@qq.com>
2025-10-03 17:13:18 +0800

    实现文件搜索工具并且使用迭代器优化

--- t/src/main.rs
+++ t/src/main.rs
@@ -1,10 +1,23 @@
-fn display_array<T: std::fmt::Debug>(arr: &[T]) {
-    println!("{:?}", arr);
-}
-fn main() {
-    let arr: [i32; 3] = [1, 2, 3];
-    display_array(&arr);
+// TryFrom 和 TryInto 也被包含在 `std::prelude` 中, 因此以下引入是没必要的
+// use std::convert::TryInto;
 
-    let arr: [i32; 2] = [1, 2];
-    display_array(&arr);
+fn main() {
+    let n: i16 = 256;
+
+    // Into 特征拥有一个方法`into`,
+    // 因此 TryInto 有一个方法是 ?
+    let n: u8 = match n.try_into() {
+        Ok(n) => n,
+        Err(e) => {
+            println!(
+                "there is an error when converting: {:?}, but we catch it",
+                e.to_string()
+            );
+            0
+        }
+    };
+
+    assert_eq!(n, 0);
+
+    println!("Success!")
 }
