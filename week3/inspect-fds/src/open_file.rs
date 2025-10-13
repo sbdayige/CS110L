32f8b7d2b460916aac09666abc40568aaaa2a789
郭申申 <3501641633@qq.com>
2025-10-13 16:44:17 +0800

    完成里程碑4

--- week3/inspect-fds/src/open_file.rs
+++ week3/inspect-fds/src/open_file.rs
@@ -1,14 +1,11 @@
 use regex::Regex;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
-#[allow(unused_imports)] // TODO: delete this line for Milestone 4
 use std::{fmt, fs};
+use std::path::Path;
 
-#[allow(unused)] // TODO: delete this line for Milestone 4
-const O_WRONLY: usize = 00000001;
-#[allow(unused)] // TODO: delete this line for Milestone 4
-const O_RDWR: usize = 00000002;
-#[allow(unused)] // TODO: delete this line for Milestone 4
+const O_WRONLY: usize = 00000001;  // 只写标志
+const O_RDWR: usize = 00000002;    // 读写标志
 const COLORS: [&str; 6] = [
     "\x1B[38;5;9m",
     "\x1B[38;5;10m",
@@ -17,12 +14,10 @@ const COLORS: [&str; 6] = [
     "\x1B[38;5;13m",
     "\x1B[38;5;14m",
 ];
-#[allow(unused)] // TODO: delete this line for Milestone 4
-const CLEAR_COLOR: &str = "\x1B[0m";
+const CLEAR_COLOR: &str = "\x1B[0m";  // 清除颜色
 
-/// This enum can be used to represent whether a file is read-only, write-only, or read/write. An
-/// enum is basically a value that can be one of some number of "things."
-#[allow(unused)] // TODO: delete this line for Milestone 4
+/// 这个枚举可以用来表示文件是只读、只写还是读写的。
+/// 枚举本质上是一个值，可以是多种"事物"中的一种。
 #[derive(Debug, Clone, PartialEq)]
 pub enum AccessMode {
     Read,
@@ -32,8 +27,8 @@ pub enum AccessMode {
 
 impl fmt::Display for AccessMode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        // Match operators are very commonly used with enums in Rust. They function similar to
-        // switch statements in other languages (but can be more expressive).
+        // Match 操作符在 Rust 中经常与枚举一起使用。它们的功能类似于
+        // 其他语言中的 switch 语句（但可以更具表现力）。
         match self {
             AccessMode::Read => write!(f, "{}", "read"),
             AccessMode::Write => write!(f, "{}", "write"),
@@ -42,9 +37,9 @@ impl fmt::Display for AccessMode {
     }
 }
 
-/// Stores information about an open file on the system. Since the Linux kernel doesn't really
-/// expose much information about the open file table to userspace (cplayground uses a modified
-/// kernel), this struct contains info from both the open file table and the vnode table.
+/// 存储系统上打开文件的信息。由于 Linux 内核实际上不会向用户空间暴露
+/// 太多关于打开文件表的信息（cplayground 使用了修改过的内核），
+/// 这个结构体包含了来自打开文件表和 vnode 表的信息。
 #[derive(Debug, Clone, PartialEq)]
 pub struct OpenFile {
     pub name: String,
@@ -53,7 +48,6 @@ pub struct OpenFile {
 }
 
 impl OpenFile {
-    #[allow(unused)] // TODO: delete this line for Milestone 4
     pub fn new(name: String, cursor: usize, access_mode: AccessMode) -> OpenFile {
         OpenFile {
             name,
@@ -62,13 +56,11 @@ impl OpenFile {
         }
     }
 
-    /// This function takes a path of an open file and returns a more human-friendly name for that
-    /// file.
+    /// 这个函数接收打开文件的路径，并返回一个更易于人类理解的文件名称。
     ///
-    /// * For regular files, this will simply return the supplied path.
-    /// * For terminals (files starting with /dev/pts), this will return "<terminal>".
-    /// * For pipes (filenames formatted like pipe:[pipenum]), this will return "<pipe #pipenum>".
-    #[allow(unused)] // TODO: delete this line for Milestone 4
+    /// * 对于普通文件，将简单地返回提供的路径。
+    /// * 对于终端（以 /dev/pts 开头的文件），将返回 "<terminal>"。
+    /// * 对于管道（格式为 pipe:[pipenum] 的文件名），将返回 "<pipe #pipenum>"。
     fn path_to_name(path: &str) -> String {
         if path.starts_with("/dev/pts/") {
             String::from("<terminal>")
@@ -80,16 +72,13 @@ impl OpenFile {
         }
     }
 
-    /// This file takes the contents of /proc/{pid}/fdinfo/{fdnum} for some file descriptor and
-    /// extracts the cursor position of that file descriptor (technically, the position of the
-    /// open file table entry that the fd points to) using a regex. It returns None if the cursor
-    /// couldn't be found in the fdinfo text.
-    #[allow(unused)] // TODO: delete this line for Milestone 4
+    /// 这个函数接收某个文件描述符的 /proc/{pid}/fdinfo/{fdnum} 文件内容，
+    /// 并使用正则表达式提取该文件描述符的游标位置（从技术上讲，是 fd 指向的
+    /// 打开文件表条目的位置）。如果在 fdinfo 文本中找不到游标，则返回 None。
     fn parse_cursor(fdinfo: &str) -> Option<usize> {
-        // Regex::new will return an Error if there is a syntactical error in our regular
-        // expression. We call unwrap() here because that indicates there's an obvious problem with
-        // our code, but if this were code for a critical system that needs to not crash, then
-        // we would want to return an Error instead.
+        // Regex::new 如果正则表达式有语法错误，将返回 Error。
+        // 我们在这里调用 unwrap()，因为这表明我们的代码有明显的问题，
+        // 但如果这是一个需要不崩溃的关键系统的代码，那么我们应该返回 Error。
         let re = Regex::new(r"pos:\s*(\d+)").unwrap();
         Some(
             re.captures(fdinfo)?
@@ -100,17 +89,15 @@ impl OpenFile {
         )
     }
 
-    /// This file takes the contents of /proc/{pid}/fdinfo/{fdnum} for some file descriptor and
-    /// extracts the access mode for that open file using the "flags:" field contained in the
-    /// fdinfo text. It returns None if the "flags" field couldn't be found.
-    #[allow(unused)] // TODO: delete this line for Milestone 4
+    /// 这个函数接收某个文件描述符的 /proc/{pid}/fdinfo/{fdnum} 文件内容，
+    /// 并使用 fdinfo 文本中包含的 "flags:" 字段提取该打开文件的访问模式。
+    /// 如果找不到 "flags" 字段，则返回 None。
     fn parse_access_mode(fdinfo: &str) -> Option<AccessMode> {
-        // Regex::new will return an Error if there is a syntactical error in our regular
-        // expression. We call unwrap() here because that indicates there's an obvious problem with
-        // our code, but if this were code for a critical system that needs to not crash, then
-        // we would want to return an Error instead.
+        // Regex::new 如果正则表达式有语法错误，将返回 Error。
+        // 我们在这里调用 unwrap()，因为这表明我们的代码有明显的问题，
+        // 但如果这是一个需要不崩溃的关键系统的代码，那么我们应该返回 Error。
         let re = Regex::new(r"flags:\s*(\d+)").unwrap();
-        // Extract the flags field and parse it as octal
+        // 提取 flags 字段并将其解析为八进制
         let flags = usize::from_str_radix(re.captures(fdinfo)?.get(1)?.as_str(), 8).ok()?;
         if flags & O_WRONLY > 0 {
             Some(AccessMode::Write)
@@ -121,30 +108,43 @@ impl OpenFile {
         }
     }
 
-    /// Given a specified process and fd number, this function reads /proc/{pid}/fd/{fdnum} and
-    /// /proc/{pid}/fdinfo/{fdnum} to populate an OpenFile struct. It returns None if the pid or fd
-    /// are invalid, or if necessary information is unavailable.
+    /// 给定指定的进程和 fd 编号，此函数读取 /proc/{pid}/fd/{fdnum} 和
+    /// /proc/{pid}/fdinfo/{fdnum} 来填充 OpenFile 结构体。如果 pid 或 fd
+    /// 无效，或者必要的信息不可用，则返回 None。
     ///
-    /// (Note: whether this function returns Option or Result is a matter of style and context.
-    /// Some people might argue that you should return Result, so that you have finer grained
-    /// control over possible things that could go wrong, e.g. you might want to handle things
-    /// differently if this fails because the process doesn't have a specified fd, vs if it
-    /// fails because it failed to read a /proc file. However, that significantly increases
-    /// complexity of error handling. In our case, this does not need to be a super robust
-    /// program and we don't need to do fine-grained error handling, so returning Option is a
-    /// simple way to indicate that "hey, we weren't able to get the necessary information"
-    /// without making a big deal of it.)
-    #[allow(unused)] // TODO: delete this line for Milestone 4
+    /// (注意：这个函数返回 Option 还是 Result 是风格和上下文的问题。
+    /// 有些人可能会争辩说你应该返回 Result，这样你可以对可能出错的事情进行更细粒度的控制，
+    /// 例如，你可能希望在进程没有指定的 fd 而失败时与读取 /proc 文件失败时进行不同的处理。
+    /// 然而，这会显著增加错误处理的复杂性。在我们的情况下，这不需要是一个超级健壮的程序，
+    /// 我们也不需要进行细粒度的错误处理，所以返回 Option 是一种简单的方式来表明
+    /// "嘿，我们无法获取必要的信息"，而不必小题大做。)
     pub fn from_fd(pid: usize, fd: usize) -> Option<OpenFile> {
-        // TODO: implement for Milestone 4
-        unimplemented!();
+        // 读取 /proc/{pid}/fd/{fd} 符号链接以获取文件路径
+        let path = format!("/proc/{}/fd/{}", pid, fd);
+        let link = fs::read_link(path).ok()?;
+        let name = OpenFile::path_to_name(&link.to_string_lossy());
+        
+        // 读取 /proc/{pid}/fdinfo/{fd} 文件以获取游标和访问模式信息
+        let fdinfo_path = format!("/proc/{}/fdinfo/{}", pid, fd);
+        let fdinfo_content = fs::read_to_string(fdinfo_path).ok()?;
+        
+        // 从 fdinfo 内容中解析游标位置
+        let cursor = OpenFile::parse_cursor(&fdinfo_content)?;
+        
+        // 从 fdinfo 内容中解析访问模式
+        let access_mode = OpenFile::parse_access_mode(&fdinfo_content)?;
+        
+        Some(OpenFile { 
+            name, 
+            cursor, 
+            access_mode,
+        })
     }
 
-    /// This function returns the OpenFile's name with ANSI escape codes included to colorize
-    /// pipe names. It hashes the pipe name so that the same pipe name will always result in the
-    /// same color. This is useful for making program output more readable, since a user can
-    /// quickly see all the fds that point to a particular pipe.
-    #[allow(unused)] // TODO: delete this line for Milestone 5
+    /// 这个函数返回带有 ANSI 转义码的 OpenFile 名称，用于对管道名称进行着色。
+    /// 它对管道名称进行哈希处理，使得相同的管道名称总是产生相同的颜色。
+    /// 这对于使程序输出更易读很有用，因为用户可以快速看到指向特定管道的所有 fd。
+    #[allow(unused)] // TODO: 在 Milestone 5 中删除这一行
     pub fn colorized_name(&self) -> String {
         if self.name.starts_with("<pipe") {
             let mut hash = DefaultHasher::new();
@@ -174,7 +174,7 @@ mod test {
     fn test_openfile_from_fd() {
         let mut test_subprocess = start_c_program("./multi_pipe_test");
         let process = ps_utils::get_target("multi_pipe_test").unwrap().unwrap();
-        // Get file descriptor 0, which should point to the terminal
+        // 获取文件描述符 0，它应该指向终端
         let open_file = OpenFile::from_fd(process.pid, 0)
             .expect("Expected to get open file data for multi_pipe_test, but OpenFile::from_fd returned None");
         assert_eq!(open_file.name, "<terminal>");
@@ -187,7 +187,7 @@ mod test {
     fn test_openfile_from_fd_invalid_fd() {
         let mut test_subprocess = start_c_program("./multi_pipe_test");
         let process = ps_utils::get_target("multi_pipe_test").unwrap().unwrap();
-        // Get file descriptor 30, which should be invalid
+        // 获取文件描述符 30，它应该是无效的
         assert!(
             OpenFile::from_fd(process.pid, 30).is_none(),
             "Expected None because file descriptor 30 is invalid"
