f1af648a7eea43784b2b3e8d3583213c11f06d20
郭申申 <3501641633@qq.com>
2025-10-18 16:11:02 +0800

    实现并发的质因数分解

--- week5/farm/src/main.rs
+++ week5/farm/src/main.rs
@@ -5,10 +5,8 @@ use std::time::Instant;
 #[allow(unused_imports)]
 use std::{env, process, thread};
 
-/// Determines whether a number is prime. This function is taken from CS 110 factor.py.
-///
-/// You don't need to read or understand this code.
-#[allow(dead_code)]
+use num_cpus::get;
+
 fn is_prime(num: u32) -> bool {
     if num <= 1 {
         return false;
@@ -21,11 +19,6 @@ fn is_prime(num: u32) -> bool {
     true
 }
 
-/// Determines the prime factors of a number and prints them to stdout. This function is taken
-/// from CS 110 factor.py.
-///
-/// You don't need to read or understand this code.
-#[allow(dead_code)]
 fn factor_number(num: u32) {
     let start = Instant::now();
 
@@ -51,8 +44,6 @@ fn factor_number(num: u32) {
     println!("{} = {} [time: {:?}]", num, factors_str, start.elapsed());
 }
 
-/// Returns a list of numbers supplied via argv.
-#[allow(dead_code)]
 fn get_input_numbers() -> VecDeque<u32> {
     let mut numbers = VecDeque::new();
     for arg in env::args().skip(1) {
@@ -71,12 +62,28 @@ fn main() {
     println!("Farm starting on {} CPUs", num_threads);
     let start = Instant::now();
 
-    // TODO: call get_input_numbers() and store a queue of numbers to factor
+    let numbers_queue = Arc::new(Mutex::new(get_input_numbers()));
+    let mut threads = Vec::new();
 
-    // TODO: spawn `num_threads` threads, each of which pops numbers off the queue and calls
-    // factor_number() until the queue is empty
+    for _ in 0..num_threads {
+        let queue_clone = Arc::clone(&numbers_queue);
+        threads.push(thread::spawn(move || {
+            loop {
+                let num = {
+                    let mut queue = queue_clone.lock().unwrap();
+                    match queue.pop_front() {
+                        Some(n) => n,
+                        None => break, // No more numbers
+                    }
+                };
+                factor_number(num);
+            }
+        }));
+    }
 
-    // TODO: join all the threads you created
+    for handle in threads {
+        handle.join().unwrap();
+    }
 
     println!("Total execution time: {:?}", start.elapsed());
 }
